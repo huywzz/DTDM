8f0684d9f0caf57da01ad2e3b7a164b0c654ec30
Huy Doan <huy.doan.quoc@madison.dev>
2025-04-28 11:33:53 +0700

    first commit

---
+++ src/modules/user/user.repository.ts
@@ -0,0 +1,61 @@
+import { User } from '@/schema/user.schema';
+import { NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+
+export class UserRepository {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createByEmail(email: string, password: string) {
+    const newUser = new this.userModel({
+      email: email,
+      password,
+    });
+    return await newUser.save();
+  }
+
+  async createBySocial(email: string, fullName: string) {
+    const newUser = new this.userModel({
+      email: email,
+      fullName: fullName,
+    });
+    return await newUser.save();
+  }
+
+  async findOneUser(conditions: {}): Promise<User | null> {
+    const user = await this.userModel.findOne(conditions);
+    return (
+      user?.populate({
+        path: 'selectedPlans.plan_id',
+        model: 'WorkoutPlan',
+        select: 'image title difficulty',
+      }) || null
+    );
+  }
+
+  async updateUser(data: {}, userId) {
+    const user = await this.userModel.findByIdAndUpdate(userId, {
+      $set: data,
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async updateOne(user: User) {
+    return await user.save();
+  }
+
+  async findAll() {
+    return await this.userModel.find();
+  }
+
+  async updateManySelectedPlans(userId: Types.ObjectId, update: any, options?: any) {
+    return await this.userModel.updateOne(
+      { _id: userId },
+      { $set: { 'selectedPlans.$[].isUsing': false } },
+      options,
+    );
+  }
+}
